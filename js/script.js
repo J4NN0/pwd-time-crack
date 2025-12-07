a05d0ec974396391a0b053eb6181501f4137a07b
J4NN0 <giannofederico@gmail.com>
2025-12-07 17:43:06 +0100

    add boiler plate

---
+++ js/script.js
@@ -0,0 +1,14 @@
+document.getElementById('password').addEventListener('input', async function () {
+    const password = this.value;
+    if (password === "") {
+        document.querySelector('#hash-output span').textContent = "";
+        return;
+    }
+    const encoder = new TextEncoder();
+    const data = encoder.encode(password);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+
+    document.querySelector('#hash-output span').textContent = hashHex;
+});
